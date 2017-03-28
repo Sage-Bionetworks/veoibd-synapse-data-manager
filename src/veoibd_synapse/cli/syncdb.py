7aec99a26f8297f5c6cee97dc07d36f89881f249
Gus Dunn <w.gus.dunn@gmail.com>
2017-03-28 11:20:28 -0400

    ProjectSubjectDatabase(SubjectDatabase) is functional

--- src/veoibd_synapse/cli/syncdb.py
+++ src/veoibd_synapse/cli/syncdb.py
@@ -26,8 +26,122 @@ __email__ = "w.gus.dunn@gmail.com"
 
 
 
-# Functions
+class SubjectDatabase(object):
+    
+    """Base class for managing interactions with Synapse concerning accessing, downloading, and combining subject database files from member-sites."""
+    
+    def __init__(self, main_confs, syn):
+        """Initialize and validate basic information.
+        
+        Extra information section
+        
+        Args:
+            main_confs (dict-like): refernce to main configuration tree.
+            syn (Synapse): an active synapse connection object.
+        
+        """
+        self.main_confs = main_confs
+        self.local_sub_db = main_confs.SITE.LOCAL_PATHS.SUBJECT_DATABASE_DIR
+        self.syn = syn
+        
+    # def __repr__(self):
+    #     return "%s(main_confs=%r, syn=%r)" % (self.__class__, self.main_confs, self.syn)
 
+class ProjectSubjectDatabase(SubjectDatabase):
+    
+    """Manage interactions with Synapse concerning accessing, downloading, and combining subject database files from a single Synapse Project."""
+    
+    def __init__(self, main_confs, syn, project_id):
+        """Initialize and validate basic information.
+        
+        Extra information section
+        
+        Args:
+            main_confs (dict-like): refernce to main configuration tree.
+            syn (Synapse): an active synapse connection object.
+            project_id (str): Synapse ID for a project.
+        
+        """
+        super(ProjectSubjectDatabase, self).__init__(main_confs, syn)
+        # self.site_name = main_confs.SITE.SITE_NAME
+        self.project_id = project_id
+        self.data = Munch()
+        self.db_files = Munch()
+        
+        self.retrieve_db_file_ids()
+        self.get_db_files()
+        
+    def retrieve_db_file_ids(self):
+        """Perform SQL query and return list of ``file.id`` values that are tagged with ``is_db=='true'`` for this project."""
+        log.debug('Beginning to retrieve DB file IDs.')
+        
+        sql_query = 'SELECT id FROM file WHERE file.is_db == "true" AND file.projectId == "{proj_id}"'
+        
+        results = self.syn.query(sql_query.format(proj_id=self.project_id))['results']
+        self.data.db_file_ids = [x['file.id'] for x in results]
+        log.debug('Retrieved DB file IDs.')
+        
+    def get_db_files(self):
+        """Iterate through DB file IDs preforming ``sns.get(db_file_id)`` and storing in ``self.db_files``."""
+        log.debug('Beginning to download {num} DB files.'.format(num=len(self.data.db_file_ids)))
+        for fid in self.data.db_file_ids:
+            file_entity = self.syn.get(fid)
+            self.db_files[file_entity.properties.name.replace('.','__')] = file_entity
+        
+        log.debug('Downloaded {num} DB files.'.format(num=len(self.data.db_file_ids)))
+
+class TeamSubjectDatabase(SubjectDatabase):
+    
+    """Manage interactions with Synapse concerning accessing, downloading, and combining subject database files from all member-sites in a Team."""
+    
+    def __init__(self, main_confs, syn, team_name):
+        """Initialize and validate basic information.
+        
+        Extra information section
+        
+        Args:
+            main_confs (dict-like): refernce to main configuration tree.
+            syn (Synapse): an active synapse connection object.
+            team_name (str): the name of a Synapse Team.
+        
+        """
+        super(TeamSubjectDatabase, self).__init__(main_confs, syn)
+        self.team_name = team_name
+        self.team_info = self.syn.getTeam(team_name)
+        self.project_ids = self.retrieve_team_project_ids()
+
+    def retrieve_team_project_ids(self):
+        """Retrieve info for all projects shared with ``self.team_name``.
+        
+        Uses the REST API and results are a multilevel dict-like object with two keys:
+            - results (list of info dicts)
+            - totalNumberOfResults (int)
+        
+        Returns:
+            None
+        """
+        team_id = self.team_info['id']
+        team_projects = self.syn.restGET('/projects/TEAM_PROJECTS/team/{team_id}'.format(team_id=team_id))
+        
+        proj_ids = []
+        for proj in team_projects['results']:
+            proj_ids.append(proj['id'])
+            
+        return proj_ids
+        
+    def build_project_dbs(self):
+        """Iterate through project IDs building DB tables from each, storing the restults.
+        
+        Stored as ``ProjectSubjectDatabase`` objects
+        """
+        
+        for proj_id in self.project_ids:
+            pass
+            
+    
+
+# Functions
+        
 
 
 
